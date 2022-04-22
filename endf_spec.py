8db05f0c7bd7eb6e9114a354bac422bb8728ce83
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-04-22 10:24:06 +0200

    further dev and restructuring

---
+++ endf_spec.py
@@ -0,0 +1,15 @@
+endf_spec_mf1_mt451 = \
+"""
+[MAT, 1,451/ ZA, AWR, LRP, LFI, NLIB, NMOD]HEAD
+[MAT, 1,451/ ELIS, STA, LIS, LISO, 0, NFOR]CONT
+[MAT, 1,451/ AWI, EMAX, LREL, 0, NSUB, NVER]CONT
+[MAT, 1,451/ TEMP, 0.0, LDRV, 0, NWD, NXC]CONT 
+"""
+
+endf_spec_mf3_mt = \
+"""
+[MAT, 3, MT/ ZA, AWR, 0, 0, 0, 0] HEAD
+[MAT, 3, MT/ QM, QI, 0, LR, NR, NP / E / xs]TAB1 (xstable)
+"""
+
+
---
+++ utils.py
@@ -0,0 +1,32 @@
+def fortstr2float(valstr, blank=None):
+    digitchars = (str(i) for i in range(10))
+    for i, c in enumerate(valstr):
+        if i>0 and (c == '+' or c == '-'):
+            if valstr[i-1] in digitchars:    
+                return float(valstr[:i] + 'E' + valstr[i:])
+    return float(valstr)
+
+def float2fortstr(val, width=11):
+    av = abs(val)
+    if av >= 1e-9 and av < 1e10:
+        nexp = 1
+    elif av >= 1e-99 and av < 1e100: 
+        nexp = 2
+    elif av == 0.0:
+        nexp = 1
+    else:
+        nexp = 3
+    ndec = width - nexp - 4  
+    fmtstr = ('{:.' + str(ndec) + 'E}')
+    ss = fmtstr.format(val)
+    mantissa, exp = ss.split('E')
+    expsign = '+'
+    if exp[0] in ('-','+'): 
+        expsign = exp[0]
+        exp = exp[1:]
+    exp = exp[:-1].lstrip('0') + exp[-1]
+    numstr = mantissa + expsign + exp
+    numstr = numstr.rjust(width)
+    assert len(numstr) == width
+    return numstr
+

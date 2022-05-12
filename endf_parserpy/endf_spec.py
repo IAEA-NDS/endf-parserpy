ae4abc9d801780c8ed0182eca58a98f3e96355e0
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-05-12 15:43:06 +0200

    make use of DESIRED_NUMBER capability in ENDF recipe

--- endf_parserpy/endf_spec.py
+++ endf_parserpy/endf_spec.py
@@ -25,7 +25,7 @@ SEND
 spec_dic.setdefault(3, {})
 spec_dic[3] = \
 """
-[MAT, 3, MT/ ZA, AWR, 0, 0, 0, 0] HEAD
+[MAT, 3, MT/ ZA, AWR, 0, 0?, 0, 0] HEAD
 [MAT, 3, MT/ QM, QI, 0, LR, NR, NP / E / xs]TAB1 (xstable)
 SEND
 """

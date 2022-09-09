5be2c8389153334a4cbc2f9d84ce59c19aed5597
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-09-09 11:35:35 +0200

    implement LI=0,LTT=1 in recipe for MF14

--- endf_parserpy/endf_recipes/endf_recipe_mf14.py
+++ endf_parserpy/endf_recipes/endf_recipe_mf14.py
@@ -19,7 +19,16 @@ endif
 # Anisotropic distribution with Legendre Coefficients Representation (14.2.2, p. 205)
 if LI == 0 and LTT == 1 [lookahead=1]:
     [MAT, 14, MT/ ZA, AWR, LI, LTT, NK, NI]HEAD
-    # TODO: implement
+    for k=1 to NI:
+        [MAT, 14, MT/ EG[k] , ES[k] , 0, 0, 0, 0] CONT
+    endfor
+    for k=NI+1 to NI+(NK-NI):
+        [MAT, 14, MT/ EG[k] , ES[k] , 0, 0, NR, NE[k]/ E ] TAB2 (E_interpol[k])
+        for l=1 to NE[k]:
+            [MAT, 14, MT/ 0.0, E[k,l] , 0, 0, NL[k,l] , 0/
+                                {a[k,l,m]}{m=1 to NL[k,l]} ] LIST
+        endfor
+    endfor
 endif
 # Anisotropic distribution with Tabulated Angular Distributions
 if LI == 0 and LTT == 2 [lookahead=1]:

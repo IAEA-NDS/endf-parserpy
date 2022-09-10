46a8cb32b056029d7e71cafc84e9be687d41c78a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-09-10 23:14:20 +0200

    implement LF=7 in recipe for MF5

--- endf_parserpy/endf_recipes/endf_recipe_mf5.py
+++ endf_parserpy/endf_recipes/endf_recipe_mf5.py
@@ -30,8 +30,8 @@ for k=1 to NK:
         endif
         # Simple Maxwellian Fission Spectrum (LF=7)
         if LF==7 [lookahead=1]:
-            [MAT, 5, MT/ U, 0.0, 0, LF, NR, NP/ E / p ]TAB1
-            # TODO
+            [MAT, 5, MT/ U, 0.0, 0, LF, NR, NP/ E / p ]TAB1 (p_table)
+            [MAT, 5, MT/ 0.0, 0.0, 0, 0, NR, NE/ E / theta ]TAB1 (theta_table)
         endif
         # Evaporation Spectrum (LF=9)
         if LF==9 [lookahead=1]:

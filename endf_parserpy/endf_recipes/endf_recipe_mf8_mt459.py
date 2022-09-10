3eb6188ff8d1c07cf17427ad965f12d9b0ac9e4e
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-09-10 21:14:24 +0200

    add mf8 recipe stubs

---
+++ endf_parserpy/endf_recipes/endf_recipe_mf8_mt459.py
@@ -0,0 +1,15 @@
+############################################################
+#
+# Author(s):       Georg Schnabel
+# Email:           g.schnabel@iaea.org
+# Creation date:   2022/09/10
+# Last modified:   2022/09/10
+# License:         MIT
+# Copyright (c) 2022 International Atomic Energy Agency (IAEA)
+#
+############################################################
+
+ENDF_RECIPE_MF8_MT459 = """
+# TODO
+SEND
+"""

774cfcc7bbc55ae9b62d353d49161887fc9f4c76
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-09-11 12:51:58 +0200

    implement mf13 recipe

--- endf_parserpy/endf_recipes/__init__.py
+++ endf_parserpy/endf_recipes/__init__.py
@@ -12,6 +12,7 @@ from .endf_recipe_mf8_mt459 import ENDF_RECIPE_MF8_MT459
 from .endf_recipe_mf9 import ENDF_RECIPE_MF9
 from .endf_recipe_mf10 import ENDF_RECIPE_MF10
 from .endf_recipe_mf12 import ENDF_RECIPE_MF12
+from .endf_recipe_mf13 import ENDF_RECIPE_MF13
 from .endf_recipe_mf14 import ENDF_RECIPE_MF14
 from .endf_recipe_mf15 import ENDF_RECIPE_MF15
 
@@ -42,6 +43,7 @@ __defdic(endf_recipe_dictionary, ( 8, 459), ENDF_RECIPE_MF8_MT459)
 __defdic(endf_recipe_dictionary, ( 9,    ), ENDF_RECIPE_MF9)
 __defdic(endf_recipe_dictionary, (10,    ), ENDF_RECIPE_MF10)
 __defdic(endf_recipe_dictionary, (12,    ), ENDF_RECIPE_MF12)
+__defdic(endf_recipe_dictionary, (13,    ), ENDF_RECIPE_MF13)
 __defdic(endf_recipe_dictionary, (14,    ), ENDF_RECIPE_MF14)
 __defdic(endf_recipe_dictionary, (15,    ), ENDF_RECIPE_MF15)
 

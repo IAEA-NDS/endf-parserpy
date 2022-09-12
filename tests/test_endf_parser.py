7c156e705d38883ab3086f0c139dc6910edd652a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-09-12 12:25:02 +0200

    add test for json conversion

--- tests/test_endf_parser.py
+++ tests/test_endf_parser.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import os
 import pytest
+import json
 from endf_parserpy.endf_parser import BasicEndfParser 
 from endf_parserpy.debugging_utils import smart_is_equal, compare_objects
+from endf_parserpy.user_tools import sanitize_fieldname_types
 
 
 @pytest.fixture(scope="module")
@@ -23,4 +25,12 @@ def test_endf_read_write_read_roundtrip_preserves_content(endf_file, tmp_path, m
     outfile = tmp_path / os.path.basename(endf_file)
     myBasicEndfParser.writefile(outfile, endf_dic)
     endf_dic2 = myBasicEndfParser.parsefile(endf_file)
+    compare_objects(endf_dic, endf_dic2, atol=1e-6, rtol=1e-5)
+
+
+def test_endf_json_endf_roundtrip_preserves_content(endf_file, tmp_path, myBasicEndfParser):
+    endf_dic = myBasicEndfParser.parsefile(endf_file)
+    jsonstr = json.dumps(endf_dic, ensure_ascii=False)
+    endf_dic2 = json.loads(jsonstr)
+    sanitize_fieldname_types(endf_dic2)
     compare_objects(endf_dic, endf_dic2, atol=1e-6, rtol=1e-5)

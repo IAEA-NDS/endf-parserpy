3a1df4ebf9280b0731fe1f5a7ef328f6bab8665b
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-09-13 00:15:17 +0200

    introduce strlen_only arg in compare_objects fun

--- endf_parserpy/debugging_utils.py
+++ endf_parserpy/debugging_utils.py
@@ -26,7 +26,7 @@ def smart_is_equal(x, y, atol=1e-8, rtol=1e-6):
         return x == y
 
 
-def compare_objects(obj1, obj2, curpath='', atol=1e-8, rtol=1e-6):
+def compare_objects(obj1, obj2, curpath='', atol=1e-8, rtol=1e-6, strlen_only=False):
 
     if type(obj1) != type(obj2):
         raise TypeError('type mismatch found, obj1: {obj1}, obj2: {obj2}')
@@ -34,17 +34,21 @@ def compare_objects(obj1, obj2, curpath='', atol=1e-8, rtol=1e-6):
     if isinstance(obj1, dict):
         only_in_obj1 = set(obj1).difference(obj2)
         if len(only_in_obj1) > 0:
-            raise IndexError(f'at path {curpath}: only obj1 contains {only_in_obj1}') 
+            raise IndexError(f'at path {curpath}: only obj1 contains {only_in_obj1}')
         only_in_obj2 = set(obj2).difference(obj1)
         if len(only_in_obj2) > 0:
             raise IndexError(f'at path {curpath}: only obj2 contains {only_in_obj2}')
 
         for key in obj1:
-            compare_objects(obj1[key], obj2[key], '.'.join((curpath, str(key))), atol=atol, rtol=rtol)
+            compare_objects(obj1[key], obj2[key], '.'.join((curpath, str(key))),
+                    atol=atol, rtol=rtol, strlen_only=strlen_only)
 
     else:
         if isinstance(obj1, str):
-            if obj1 != obj2:
+            if strlen_only:
+                if len(obj1) != len(obj2):
+                    raise ValueError(f'at path {curpath}: string lengths differ ({obj1} != {obj2})')
+            elif obj1 != obj2:
                 raise ValueError(f'at path {curpath}: strings differ ({obj1} != {obj2})')
 
         elif hasattr(obj1, '__iter__'):
@@ -54,7 +58,8 @@ def compare_objects(obj1, obj2, curpath='', atol=1e-8, rtol=1e-6):
                 raise ValueError(f'Length mismatch at {curpath} ({len_obj1} vs {len_obj2})')
 
             for i, (subel1, subel2) in enumerate(zip(obj1, obj2)):
-                compare_objects(subel1, subel2, f'{curpath}[{str(i)}]', atol=atol, rtol=rtol)  
+                compare_objects(subel1, subel2, f'{curpath}[{str(i)}]',
+                        atol=atol, rtol=rtol, strlen_only=strlen_only)
         else:
             if not smart_is_equal(obj1, obj2, atol=atol, rtol=rtol):
-                raise ValueError(f'Value mismatch at {curpath} ({obj1} vs {obj2})') 
+                raise ValueError(f'Value mismatch at {curpath} ({obj1} vs {obj2})')

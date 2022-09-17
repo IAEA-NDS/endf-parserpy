71c00e4f50975b080770fe243d034d7ed8a0783a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-09-17 03:23:29 +0200

    fix mf34 recipe file

--- endf_parserpy/endf_recipes/endf_recipe_mf34.py
+++ endf_parserpy/endf_recipes/endf_recipe_mf34.py
@@ -14,12 +14,26 @@ ENDF_RECIPE_MF34 = """
 for k=1 to NMT1:
     (subsection[k])
         [MAT,34,MT/ 0.0, 0.0, MAT1, MT1, NL, NL1]CONT
-        for n=1 to NL: 
-            [MAT,34,MT/ 0.0, 0.0, L[n] , L1[n], LCT, NI[n] ]CONT
-            for j=1 to NI[n]:
-                [MAT,34,MT/ 0.0, 0.0, LS[n,j] , LB[n,j], NT[n,j] , NE[n,j] / 
-                    {Data[n,j,q]}{q=1 to NT[n,j]} ] LIST
-            endfor
+        for n=1 to NL:
+            if MT == MT1:
+                for j=n to NL1:
+                    # QUESTION: The NI variable is quite pointless, isn't it?
+                    # According to ENDF manual: NI ...Number of LIST records in this
+                    # sub-subsection.
+                    # If a sub-subsection is given by the two records below, there
+                    # is always exactly one LIST record.
+                    # TODO: Need to check the meaning of NI.
+                    [MAT,34,MT/ 0.0, 0.0, L[n] , L1[n,j], LCT, NI[n] ]CONT
+                    [MAT,34,MT/ 0.0, 0.0, LS[n,j] , LB[n,j], NT[n,j] , NE[n,j] /
+                        {Data[n,j,q]}{q=1 to NT[n,j]} ] LIST
+                endfor
+            else:
+                for j=1 to NL1:
+                    [MAT,34,MT/ 0.0, 0.0, L[n] , L1[n,j], LCT, NI[n] ]CONT
+                    [MAT,34,MT/ 0.0, 0.0, LS[n,j] , LB[n,j], NT[n,j] , NE[n,j] /
+                        {Data[n,j,q]}{q=1 to NT[n,j]} ] LIST
+                endfor
+            endif
         endfor
     (/subsection[k])
 endfor

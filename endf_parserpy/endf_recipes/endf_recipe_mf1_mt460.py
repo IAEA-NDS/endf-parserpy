0aab46b4adffee7553c8062e422267583cde4afb
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-11-20 23:54:59 +0100

    add mf1 mt460 recipe stub

---
+++ endf_parserpy/endf_recipes/endf_recipe_mf1_mt460.py
@@ -0,0 +1,15 @@
+############################################################
+#
+# Author(s):       Georg Schnabel
+# Email:           g.schnabel@iaea.org
+# Creation date:   2022/11/20
+# Last modified:   2022/11/20
+# License:         MIT
+# Copyright (c) 2022 International Atomic Energy Agency (IAEA)
+#
+############################################################
+
+ENDF_RECIPE_MF1_MT460 = """
+
+SEND
+"""

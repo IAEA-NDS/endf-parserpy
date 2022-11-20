e8312e087552a23a76e669a3a8d346d7410eab88
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-11-21 00:25:30 +0100

    implement endf recipe mf8 mt454 and mt459

--- endf_parserpy/endf_recipes/endf_recipe_mf8_mt459.py
+++ endf_parserpy/endf_recipes/endf_recipe_mf8_mt459.py
@@ -10,6 +10,13 @@
 ############################################################
 
 ENDF_RECIPE_MF8_MT459 = """
-# TODO
+
+[MAT, 8, MT/ ZA, AWR, LE+1, 0, 0, 0]HEAD
+[MAT, 8, MT/ E[0], 0.0, LE, 0, NN[0], NFP[0]/
+    {ZAFP[0,m], FPS[0,m], YC[0,m], DYC[0,m]}{m=1 to NFP[0]} ]LIST
+for k=1 to LE:
+    [MAT, 8, MT/ E[k], 0.0, I[k], 0, NN[k], NFP[k]/
+        {ZAFP[k,m], FPS[k,m], YC[k,m], DYC[k,m]}{m=1 to NFP[k]} ]LIST
+endfor
 SEND
 """

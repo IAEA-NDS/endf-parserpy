d50c57aeb0c4355b16cd2095c8c9aa04c1c41116
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-11-24 15:06:50 +0100

    introduce math_allclose function

--- endf_parserpy/math_utils.py
+++ endf_parserpy/math_utils.py
@@ -11,18 +11,22 @@
 from .custom_exceptions import InvalidIntegerError
 
 
-def math_op(x, y, op):
+def math_isclose(x, y, rtol=1e-5, atol=1e-8):
+    return abs(x-y) <= (atol + rtol*abs(y))
+
+
+def math_op(x, y, op, **kwargs):
     if hasattr(x, '__iter__') and hasattr(y, '__iter__'):
         if len(x) != len(y):
             raise ValueError('x and y must be of same length ' +
                              f'(len(x)={len(x)} vs len(y)={len(y)})')
-        return type(x)(op(z1, z2) for z1, z2 in zip(x, y))
+        return type(x)(op(z1, z2, **kwargs) for z1, z2 in zip(x, y))
     elif hasattr(x, '__iter__'):
-        return type(x)(op(z, y) for z in x)
+        return type(x)(op(z, y, **kwargs) for z in x)
     elif hasattr(y, '__iter__'):
-        return type(y)(op(x, z) for z in y)
+        return type(y)(op(x, z, **kwargs) for z in y)
     else:
-        return op(x, y)
+        return op(x, y, **kwargs)
 
 
 def math_neg(x):
@@ -54,3 +58,7 @@ def math_add(x, y):
 
 def math_sub(x, y):
     return math_op(x, y, lambda a, b: a-b)
+
+
+def math_allclose(x, y, rtol=1e-5, atol=1e-8):
+    return math_op(x, y, math_isclose, rtol=rtol, atol=atol)

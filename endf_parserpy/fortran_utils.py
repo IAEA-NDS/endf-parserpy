9bcc3c0dd7a4e1c7c73c15aba70c35701962453b
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-11-30 10:18:56 +0100

    simplify float2fortstr and helpferfuns

--- endf_parserpy/fortran_utils.py
+++ endf_parserpy/fortran_utils.py
@@ -56,10 +56,8 @@ def float2basicnumstr(val, **write_opts):
     # -1 due to a minus sign slot
     # -1 due to the decimal point
     waste_space = 2
-    intpos = 1
     if abuse_signpos and val > 0:
         waste_space -= 1
-        intpos = 0
     should_skip_zero = skip_intzero and intpart == 0
     if should_skip_zero:
         effwidth += 1
@@ -68,14 +66,17 @@ def float2basicnumstr(val, **write_opts):
     floatwidth = effwidth - waste_space - len_intpart
     if floatwidth > 0 and not is_integer:
         numstr = f'{{:{effwidth}.{floatwidth}f}}'.format(val)
-    elif log10(abs(intpart)) < width:
-        numstr = f'{{:{effwidth}d}}'.format(int(val))
+        if should_skip_zero:
+            dotpos = numstr.index('.')
+            numstr = numstr[:dotpos-1] + numstr[dotpos:]
     else:
-        raise ValueError('cannot represent the number')
-    if should_skip_zero:
-        numstr = numstr[:intpos] + numstr[intpos+1:]
-    if '.' in numstr:
-        numstr = numstr.rstrip('0').rjust(width)
+        numstr = '{:d}'.format(int(val))
+        if val > 0 and not abuse_signpos:
+            numstr = ' ' + numstr
+        if len(numstr) <= width-2:
+            numstr += '.'
+            numstr = numstr.ljust(width, '0')
+    numstr = numstr.rjust(width)
     return numstr
 
 
@@ -111,37 +112,40 @@ def float2expformstr(val, **write_opts):
     return sign_str + mantissa_str + expsymb_str + exposign_str + exponent_str
 
 
-def is_noexpform_more_precise(val, **write_opts):
-    width = write_opts.get('width', 11)
-    skip_intzero = write_opts.get('skip_intzero', False)
-    abuse_signpos = write_opts.get('abuse_signpos', False)
-    keep_E = write_opts.get('keep_E', False)
-    if val == 0:
-        return True
-    digit_advantage = 2
-    if skip_intzero and abs(int(val)) < 1:
-        digit_advantage += 1
-    if keep_E:
-        digit_advantage += 1
-    expo = log10(abs(val)) if val != 0 else 0
-    if expo < 0:
-        return digit_advantage + expo >= 0
-    else:
-        sign_inc = 1 if val > 0 and abuse_signpos else 0
-        int_inc = 1 if int(val) == val else 0
-        # -1 for decimal point
-        # -1 for sign slot
-        num_digits = width - 2 + sign_inc + int_inc
-        return num_digits > expo
+def count_signif_digits(valstr):
+    num_signif = 0
+    in_signif = False
+    zero_acc = 0
+    for c in valstr:
+        if not in_signif and c.isdigit() and c != '0':
+            in_signif = True
+        if in_signif:
+            if c.isdigit():
+                if c != '0':
+                    num_signif += 1 + zero_acc
+                    zero_acc = 0
+                else:
+                    zero_acc += 1
+            elif c != '.':
+                break
+    return num_signif
 
 
 def float2fortstr(val, **write_opts):
+    width = write_opts.get('width', 11)
     prefer_noexp = write_opts.get('prefer_noexp', False)
-    noexp_more_precise = is_noexpform_more_precise(val, **write_opts)
-    if prefer_noexp and noexp_more_precise:
-        return float2basicnumstr(val, **write_opts)
-    else:
-        return float2expformstr(val, **write_opts)
+    if prefer_noexp:
+        valstr_basic = float2basicnumstr(val, **write_opts)
+    valstr_exp = float2expformstr(val, **write_opts)
+    if prefer_noexp and len(valstr_basic) <= width:
+        num_signif_basic = count_signif_digits(valstr_basic)
+        num_signif_exp = count_signif_digits(valstr_exp)
+        if num_signif_basic >= num_signif_exp:
+            if '.' in valstr_basic:
+                valstr_basic = valstr_basic.rstrip('0').rstrip('.')
+            valstr_basic = valstr_basic.rjust(width)
+            return valstr_basic
+    return valstr_exp
 
 
 def read_fort_floats(line, n=6, blank=None, **read_opts):

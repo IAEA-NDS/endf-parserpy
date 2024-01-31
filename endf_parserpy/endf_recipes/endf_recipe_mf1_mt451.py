83996c4115e034b40335115ac4e1418adce2d7d5
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-01-31 17:47:03 +0100

    update MF1/MT451 recipe with better variable assignments
    and also update test_endf_construction.py accordingly

--- endf_parserpy/endf_recipes/endf_recipe_mf1_mt451.py
+++ endf_parserpy/endf_recipes/endf_recipe_mf1_mt451.py
@@ -15,7 +15,14 @@ ENDF_RECIPE_MF1_MT451 = """
 [MAT, 1,451/ ELIS, STA, LIS, LISO, 0, NFOR]CONT
 [MAT, 1,451/ AWI, EMAX, LREL, 0, NSUB, NVER]CONT
 [MAT, 1,451/ TEMP, 0.0, LDRV, 0, NWD, NXC]CONT
-for i=1 to NWD:
+
+[MAT, 1,451/ ZSYMAM{11}, ALAB{11}, EDATE{10}, {1}, AUTH{33} ]TEXT
+[MAT, 1,451/ {1}, REF{21}, DDATE{10}, {1},
+             RDATE{10}, {12}, ENDATE{8}, {3} ]TEXT
+for i=1 to 3:
+    [MAT, 1,451/ HSUB[i]] TEXT
+endfor
+for i=1 to NWD-5:
     [MAT, 1,451/ DESCRIPTION[i]]TEXT
 endfor
 for i=1 to NXC:

2ef7f43186f44ae5ab9018e2b4160c3013247d94
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-02-16 06:22:12 +0100

    add pytest file test_endf_parser_options.py

---
+++ tests/test_endf_parser_options.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from endf_parserpy import EndfParser
+from endf_parserpy.debugging_utils import compare_objects
+from endf_parserpy.user_tools import list_parsed_sections
+
+
+def test_parsefile_include_single_mf_option():
+    parser = EndfParser()
+    endf_file = Path(__file__).parent.joinpath("testdata", "n_2925_29-Cu-63.endf")
+    result1 = parser.parsefile(endf_file, include=[3])
+    result2 = parser.parsefile(endf_file, include=3)
+    compare_objects(result1, result2)
+
+
+def test_parsefile_exclude_single_mf_option():
+    parser = EndfParser()
+    endf_file = Path(__file__).parent.joinpath("testdata", "n_2925_29-Cu-63.endf")
+    result1 = parser.parsefile(endf_file, exclude=[3])
+    result2 = parser.parsefile(endf_file, exclude=3)
+    compare_objects(result1, result2)
+
+
+def test_parsefile_include_option_with_mf_mt():
+    parser = EndfParser()
+    endf_file = Path(__file__).parent.joinpath("testdata", "n_2925_29-Cu-63.endf")
+    result1 = parser.parsefile(endf_file, include=[1, (3, 1), 6])
+    result2 = parser.parsefile(endf_file, include=([3, 1], 1, 6))
+    parsed_sections = list_parsed_sections(result1)
+    assert all(p[0] in (1, 3, 6) for p in parsed_sections)
+    assert all(p[1] == 1 for p in parsed_sections if p[0] == 3)
+    compare_objects(result1, result2)
+
+
+def test_parsefile_exclude_option_with_mf_mt():
+    parser = EndfParser()
+    endf_file = Path(__file__).parent.joinpath("testdata", "n_2925_29-Cu-63.endf")
+    result1 = parser.parsefile(endf_file, exclude=[1, (3, 1), 6])
+    result2 = parser.parsefile(endf_file, exclude=([3, 1], 1, 6))
+    parsed_sections = list_parsed_sections(result1)
+    assert not any(p[0] in (1, 6) for p in parsed_sections)
+    assert not any(p[1] == 1 for p in parsed_sections if p[0] == 3)
+    compare_objects(result1, result2)

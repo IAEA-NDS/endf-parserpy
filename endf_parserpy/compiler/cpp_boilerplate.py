8aea86e8a63eb3d27b08592d34b276fecbc97c00
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-04-15 21:37:52 +0200

    fix: now read floats with eE char correctly

--- endf_parserpy/compiler/cpp_boilerplate.py
+++ endf_parserpy/compiler/cpp_boilerplate.py
@@ -3,7 +3,7 @@
 # Author(s):       Georg Schnabel
 # Email:           g.schnabel@iaea.org
 # Creation date:   2024/04/12
-# Last modified:   2024/04/14
+# Last modified:   2024/04/15
 # License:         MIT
 # Copyright (c) 2024 International Atomic Energy Agency (IAEA)
 #
@@ -39,9 +39,9 @@ def module_header():
                     if (c=='+' || c=='-') {
                         tbuf[j++] = 'e';
                         in_exponent = true;
+                    } else if (c=='e' || c=='E') {
+                        in_exponent = true;
                     }
-                } else if (c=='e' || c=='E') {
-                    in_exponent = true;
                 }
             } else {
                 if (c == '.' || (c >= '0' && c <= '9')) {

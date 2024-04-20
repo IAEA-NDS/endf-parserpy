bd9644fb75c65810b2a31e92f96dfec1e4e3abf6
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-04-20 15:40:49 +0200

    add inplace version of transform_nodes

--- endf_parserpy/compiler/expr_utils/tree_walkers.py
+++ endf_parserpy/compiler/expr_utils/tree_walkers.py
@@ -3,7 +3,7 @@
 # Author(s):       Georg Schnabel
 # Email:           g.schnabel@iaea.org
 # Creation date:   2024/03/28
-# Last modified:   2024/03/28
+# Last modified:   2024/04/20
 # License:         MIT
 # Copyright (c) 2024 International Atomic Energy Agency (IAEA)
 #
@@ -12,9 +12,20 @@
 from lark.tree import Tree
 
 
-def transform_nodes(node, func, *args, **kwargs):
+def _transform_nodes(node, func, inplace, *args, **kwargs):
     if isinstance(node, Tree):
-        node = node.copy()
-        tc = [transform_nodes(v, func, *args, **kwargs) for v in node.children]
+        if not inplace:
+            node = node.copy()
+        tc = [
+            _transform_nodes(v, func, inplace, *args, **kwargs) for v in node.children
+        ]
         node.children = [v for v in tc if v is not None]
     return func(node, *args, **kwargs)
+
+
+def transform_nodes(node, func, *args, **kwargs):
+    return _transform_nodes(node, func, False, *args, **kwargs)
+
+
+def transform_nodes_inplace(node, func, *args, **kwargs):
+    return _transform_nodes(node, func, True, *args, **kwargs)

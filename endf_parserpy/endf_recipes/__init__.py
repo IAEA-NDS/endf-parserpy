0bbdad96a19f3719ca92d1f65c0879f5c2d1dcba
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-04-26 06:49:41 +0200

    introduce convenience fun to get recipe dictionary

---
+++ endf_parserpy/endf_recipes/__init__.py
@@ -0,0 +1,17 @@
+from .endf6 import endf_recipe_dictionary as endf6_recipes
+from .jendl import endf_recipe_dictionary as jendl_recipes
+
+
+_FORMAT_DICT = {
+    "endf6": endf6_recipes,
+    "jendl": jendl_recipes,
+}
+
+
+def get_recipe_dict(format_spec):
+    if format_spec not in _FORMAT_DICT:
+        raise TypeError(
+            f"There isn't any format specification "
+            + "associated with the name `{format_spec}`"
+        )
+    return _FORMAT_DICT[format_spec]

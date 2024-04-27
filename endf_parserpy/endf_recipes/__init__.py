3904c7c208893d236033031c5fcc154aab6d919d
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-04-27 15:39:53 +0200

    remove redundancy in JENDL recipes definition

--- endf_parserpy/endf_recipes/__init__.py
+++ endf_parserpy/endf_recipes/__init__.py
@@ -15,3 +15,7 @@ def get_recipe_dict(format_spec):
             + "associated with the name `{format_spec}`"
         )
     return _FORMAT_DICT[format_spec]
+
+
+def list_recipe_flavors():
+    return tuple(_FORMAT_DICT.keys())
--- endf_parserpy/endf_recipes/jendl/__init__.py
+++ endf_parserpy/endf_recipes/jendl/__init__.py
@@ -1,85 +1,10 @@
+from copy import deepcopy
 from ..utils import defdic
-
-# standard recipes as given in the ENDF-6 manual
-from ..endf6.endf_recipe_mf0_mt0 import ENDF_RECIPE_MF0_MT0_TAPEHEAD
-from ..endf6.endf_recipe_mf1_mt451 import ENDF_RECIPE_MF1_MT451
-from ..endf6.endf_recipe_mf1_mt452 import ENDF_RECIPE_MF1_MT452
-from ..endf6.endf_recipe_mf1_mt455 import ENDF_RECIPE_MF1_MT455
-from ..endf6.endf_recipe_mf1_mt456 import ENDF_RECIPE_MF1_MT456
-from ..endf6.endf_recipe_mf1_mt458 import ENDF_RECIPE_MF1_MT458
-from ..endf6.endf_recipe_mf1_mt460 import ENDF_RECIPE_MF1_MT460
-from ..endf6.endf_recipe_mf2_mt151 import ENDF_RECIPE_MF2_MT151
-from ..endf6.endf_recipe_mf3 import ENDF_RECIPE_MF3
-from ..endf6.endf_recipe_mf4 import ENDF_RECIPE_MF4
-from ..endf6.endf_recipe_mf5 import ENDF_RECIPE_MF5
-from ..endf6.endf_recipe_mf6 import ENDF_RECIPE_MF6
-from ..endf6.endf_recipe_mf7 import ENDF_RECIPE_MF7_MT2, ENDF_RECIPE_MF7_MT4
-from ..endf6.endf_recipe_mf8 import ENDF_RECIPE_MF8
-from ..endf6.endf_recipe_mf8_mt454 import ENDF_RECIPE_MF8_MT454
-
-# from ..endf6.endf_recipe_mf8_mt457 import ENDF_RECIPE_MF8_MT457
-from ..endf6.endf_recipe_mf8_mt459 import ENDF_RECIPE_MF8_MT459
-from ..endf6.endf_recipe_mf9 import ENDF_RECIPE_MF9
-from ..endf6.endf_recipe_mf10 import ENDF_RECIPE_MF10
-from ..endf6.endf_recipe_mf12 import ENDF_RECIPE_MF12
-from ..endf6.endf_recipe_mf13 import ENDF_RECIPE_MF13
-from ..endf6.endf_recipe_mf14 import ENDF_RECIPE_MF14
-from ..endf6.endf_recipe_mf15 import ENDF_RECIPE_MF15
-from ..endf6.endf_recipe_mf23 import ENDF_RECIPE_MF23
-from ..endf6.endf_recipe_mf26 import ENDF_RECIPE_MF26
-from ..endf6.endf_recipe_mf27 import ENDF_RECIPE_MF27
-from ..endf6.endf_recipe_mf28 import ENDF_RECIPE_MF28
-from ..endf6.endf_recipe_mf31_mt452_455_456 import ENDF_RECIPE_MF31_MT452_455_456
-from ..endf6.endf_recipe_mf32 import ENDF_RECIPE_MF32
-from ..endf6.endf_recipe_mf33 import ENDF_RECIPE_MF33
-from ..endf6.endf_recipe_mf34 import ENDF_RECIPE_MF34
-from ..endf6.endf_recipe_mf35 import ENDF_RECIPE_MF35
-from ..endf6.endf_recipe_mf40 import ENDF_RECIPE_MF40
+from ..endf6 import endf_recipe_dictionary as base_recipe_dictionary
 
 # JENDL-5 specific alterations
 from .endf_recipe_mf8_mt457 import ENDF_RECIPE_MF8_MT457
 
 
-# create a nested dictionary with all the
-# endf recipes to read and write the various mf/mt sections.
-# both (MF,) and (MF,-1) defines a recipe for the complete MF section.
-# The second option allows to define special recipes for MT sections,
-# see e.g. MF8 recipes below.
-endf_recipe_dictionary = {}
-defdic(endf_recipe_dictionary, (0, 0), ENDF_RECIPE_MF0_MT0_TAPEHEAD)
-defdic(endf_recipe_dictionary, (1, 451), ENDF_RECIPE_MF1_MT451)
-defdic(endf_recipe_dictionary, (1, 452), ENDF_RECIPE_MF1_MT452)
-defdic(endf_recipe_dictionary, (1, 455), ENDF_RECIPE_MF1_MT455)
-defdic(endf_recipe_dictionary, (1, 456), ENDF_RECIPE_MF1_MT456)
-defdic(endf_recipe_dictionary, (1, 458), ENDF_RECIPE_MF1_MT458)
-defdic(endf_recipe_dictionary, (1, 460), ENDF_RECIPE_MF1_MT460)
-defdic(endf_recipe_dictionary, (2, 151), ENDF_RECIPE_MF2_MT151)
-defdic(endf_recipe_dictionary, (3,), ENDF_RECIPE_MF3)
-defdic(endf_recipe_dictionary, (4,), ENDF_RECIPE_MF4)
-defdic(endf_recipe_dictionary, (5,), ENDF_RECIPE_MF5)
-defdic(endf_recipe_dictionary, (6,), ENDF_RECIPE_MF6)
-defdic(endf_recipe_dictionary, (7, 2), ENDF_RECIPE_MF7_MT2)
-defdic(endf_recipe_dictionary, (7, 4), ENDF_RECIPE_MF7_MT4)
-defdic(endf_recipe_dictionary, (8, -1), ENDF_RECIPE_MF8)
-defdic(endf_recipe_dictionary, (8, 454), ENDF_RECIPE_MF8_MT454)
+endf_recipe_dictionary = deepcopy(base_recipe_dictionary)
 defdic(endf_recipe_dictionary, (8, 457), ENDF_RECIPE_MF8_MT457)
-defdic(endf_recipe_dictionary, (8, 459), ENDF_RECIPE_MF8_MT459)
-defdic(endf_recipe_dictionary, (9,), ENDF_RECIPE_MF9)
-defdic(endf_recipe_dictionary, (10,), ENDF_RECIPE_MF10)
-defdic(endf_recipe_dictionary, (12,), ENDF_RECIPE_MF12)
-defdic(endf_recipe_dictionary, (13,), ENDF_RECIPE_MF13)
-defdic(endf_recipe_dictionary, (14,), ENDF_RECIPE_MF14)
-defdic(endf_recipe_dictionary, (15,), ENDF_RECIPE_MF15)
-defdic(endf_recipe_dictionary, (23,), ENDF_RECIPE_MF23)
-defdic(endf_recipe_dictionary, (26,), ENDF_RECIPE_MF26)
-defdic(endf_recipe_dictionary, (27,), ENDF_RECIPE_MF27)
-defdic(endf_recipe_dictionary, (28,), ENDF_RECIPE_MF28)
-defdic(endf_recipe_dictionary, (31, 452), ENDF_RECIPE_MF31_MT452_455_456)
-defdic(endf_recipe_dictionary, (31, 455), ENDF_RECIPE_MF31_MT452_455_456)
-defdic(endf_recipe_dictionary, (31, 456), ENDF_RECIPE_MF31_MT452_455_456)
-defdic(endf_recipe_dictionary, (31,), ENDF_RECIPE_MF31_MT452_455_456)
-defdic(endf_recipe_dictionary, (32,), ENDF_RECIPE_MF32)
-defdic(endf_recipe_dictionary, (33,), ENDF_RECIPE_MF33)
-defdic(endf_recipe_dictionary, (34,), ENDF_RECIPE_MF34)
-defdic(endf_recipe_dictionary, (35,), ENDF_RECIPE_MF35)
-defdic(endf_recipe_dictionary, (40,), ENDF_RECIPE_MF40)

a81cc3910f3d866750bb37bbabc3a08c6dc5170c
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-04-29 08:28:05 +0200

    add auxiliary func is_if_condition

--- endf_parserpy/compiler/node_checks.py
+++ endf_parserpy/compiler/node_checks.py
@@ -3,7 +3,7 @@
 # Author(s):       Georg Schnabel
 # Email:           g.schnabel@iaea.org
 # Creation date:   2024/04/12
-# Last modified:   2024/04/15
+# Last modified:   2024/04/29
 # License:         MIT
 # Copyright (c) 2024 International Atomic Energy Agency (IAEA)
 #
@@ -71,6 +71,10 @@ def is_for_loop(node):
 
 def is_if_clause(node):
     return isinstance(node, Tree) and node.data == "if_clause"
+
+
+def is_if_condition(node):
+    return isinstance(node, Tree) and node.data == "if_condition"
 
 
 def is_abbreviation(node):

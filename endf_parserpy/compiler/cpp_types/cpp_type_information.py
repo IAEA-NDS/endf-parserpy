a508ec7a08dcda3441bd7d03bf3ff321e9495bfd
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-05-01 14:07:42 +0200

    introduce LazyModuleClass to avoid circular imports

--- endf_parserpy/compiler/cpp_types/cpp_type_information.py
+++ endf_parserpy/compiler/cpp_types/cpp_type_information.py
@@ -3,20 +3,40 @@
 # Author(s):       Georg Schnabel
 # Email:           g.schnabel@iaea.org
 # Creation date:   2024/04/25
-# Last modified:   2024/04/25
+# Last modified:   2024/05/01
 # License:         MIT
 # Copyright (c) 2024 International Atomic Energy Agency (IAEA)
 #
 ############################################################
 
-from .cpp_type_scalar import CppTypeScalar
-from .cpp_type_nested_vector import CppTypeNestedVector
-from .cpp_type_matrix2d import CppTypeMatrix2d
+import importlib
+
+
+_THIS_PCKG = "endf_parserpy.compiler.cpp_types"
+
+
+# to avoid nasty circular imports issues, this class
+# only loads a module when the class defined
+# therein is currently needed
+class LazyModuleClass:
+
+    def __init__(self, module_name, class_name):
+        self.module_name = module_name
+        self.module = None
+        self.class_name = class_name
+
+    def __call__(self):
+        if self.module is None:
+            self.module = importlib.import_module(self.module_name)
+        return getattr(self.module, self.class_name)
+
 
 VARTYPE_INFO = {
-    "Matrix2d": CppTypeMatrix2d,
-    "NestedVector": CppTypeNestedVector,
-    "Scalar": CppTypeScalar,
+    "Matrix2d": LazyModuleClass(f"{_THIS_PCKG}.cpp_type_matrix2d", "CppTypeMatrix2d"),
+    "NestedVector": LazyModuleClass(
+        f"{_THIS_PCKG}.cpp_type_nested_vector", "CppTypeNestedVector"
+    ),
+    "Scalar": LazyModuleClass(f"{_THIS_PCKG}.cpp_type_scalar", "CppTypeScalar"),
 }
 
 
@@ -25,19 +45,19 @@ def get_vartype_names():
 
 
 def get_vartype_definition(vartype_name):
-    return VARTYPE_INFO[vartype_name].definition
+    return VARTYPE_INFO[vartype_name]().definition
 
 
 def get_query_module(vartype_name):
-    return VARTYPE_INFO[vartype_name].query
+    return VARTYPE_INFO[vartype_name]().query
 
 
 def get_assign_module(vartype_name):
-    return VARTYPE_INFO[vartype_name].assign
+    return VARTYPE_INFO[vartype_name]().assign
 
 
 def get_vartype_module(vartype_name):
-    return VARTYPE_INFO[vartype_name]
+    return VARTYPE_INFO[vartype_name]()
 
 
 # tuples convenient for loops

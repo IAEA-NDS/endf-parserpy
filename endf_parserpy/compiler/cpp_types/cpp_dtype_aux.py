7983195adcc985261ff9430d299994bdee91d663
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-05-04 09:53:36 +0200

    improve internals of cpp_dtype_aux.py

--- endf_parserpy/compiler/cpp_types/cpp_dtype_aux.py
+++ endf_parserpy/compiler/cpp_types/cpp_dtype_aux.py
@@ -3,58 +3,31 @@
 # Author(s):       Georg Schnabel
 # Email:           g.schnabel@iaea.org
 # Creation date:   2024/05/01
-# Last modified:   2024/05/01
+# Last modified:   2024/05/04
 # License:         MIT
 # Copyright (c) 2024 International Atomic Energy Agency (IAEA)
 #
 ############################################################
 
 
-def map_dtype(dtype):
-    if dtype == float:
-        dtype = "double"
-    elif dtype == int:
-        dtype = "int"
-    elif dtype == str:
-        dtype = "std::string"
-    elif dtype == "loopvartype":
-        dtype = "int"
-    elif dtype == "intvec":
-        dtype = "std::vector<int>"
-    elif dtype == "floatvec":
-        dtype = "std::vector<double>"
-    else:
-        raise TypeError(f"unknown dtype {dtype}")
-    return dtype
+_DTYPE_INFO = {
+    # tuple organization: string representation, cpp type, dtype index
+    float: ("double", "double", 0),
+    int: ("int", "int", 1),
+    str: ("string", "std::string", 2),
+    "loopvartype": ("int", "int", 3),
+    "intvec": ("intvec", "std::vector<int>", 4),
+    "floatvec": ("floatvec", "std::vector<double>", 5),
+}
 
 
 def get_dtype_str(dtype):
-    if dtype == float:
-        return "double"
-    elif dtype == int:
-        return "int"
-    elif dtype == str:
-        return "string"
-    elif dtype == "loopvartype":
-        return "int"
-    elif dtype == "intvec":
-        return "intvec"
-    elif dtype == "floatvec":
-        return "floatvec"
-    raise TypeError(f"unknown dtype {dtype}")
+    return _DTYPE_INFO[dtype][0]
+
+
+def map_dtype(dtype):
+    return _DTYPE_INFO[dtype][1]
 
 
 def get_dtype_idx(dtype):
-    if dtype == float:
-        return 0
-    elif dtype == int:
-        return 1
-    elif dtype == str:
-        return 2
-    elif dtype == "loopvartype":
-        return 3
-    elif dtype == "intvec":
-        return 4
-    elif dtype == "floatvec":
-        return 5
-    raise TypeError(f"unknown dtype {dtype}")
+    return _DTYPE_INFO[dtype][2]

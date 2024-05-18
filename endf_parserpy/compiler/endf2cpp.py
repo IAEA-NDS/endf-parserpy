bc2944f4fe931b7bb1942ca097c6481f0d096be0
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-05-18 11:30:30 +0200

    incoporate cpp write code skeleton in code generation

--- endf_parserpy/compiler/endf2cpp.py
+++ endf_parserpy/compiler/endf2cpp.py
@@ -11,18 +11,21 @@
 
 from . import cpp_boilerplate
 from .code_generator_parsing import generate_all_cpp_parsefuns_code
+from .code_generator_writing import generate_all_cpp_writefuns_code
 
 
 def generate_cpp_module_code(recipes, module_name):
     parsefuns_code, parsefuns_pybind_glue = generate_all_cpp_parsefuns_code(
         recipes, module_name
     )
-
-    module_header = cpp_boilerplate.module_header()
-    pybind_glue = parsefuns_pybind_glue
-    pybind_glue = cpp_boilerplate.register_pybind_module(
-        module_name, parsefuns_pybind_glue
+    writefuns_code, writefuns_pybind_glue = generate_all_cpp_writefuns_code(
+        recipes, module_name
     )
 
-    code = module_header + parsefuns_code + pybind_glue
+    funs_code = parsefuns_code + writefuns_code
+    pybind_glue = parsefuns_pybind_glue + writefuns_pybind_glue
+
+    module_header = cpp_boilerplate.module_header()
+    pybind_glue = cpp_boilerplate.register_pybind_module(module_name, pybind_glue)
+    code = module_header + funs_code + pybind_glue
     return code

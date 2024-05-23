ba33b88bf9493858fe2597e4ef32831c79432dcc
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-05-24 00:27:09 +0200

    fix functions to read values from `endf_dict` in `code_generator_writing_core` module

--- endf_parserpy/compiler/code_generator_writing_core.py
+++ endf_parserpy/compiler/code_generator_writing_core.py
@@ -3,23 +3,54 @@
 # Author(s):       Georg Schnabel
 # Email:           g.schnabel@iaea.org
 # Creation date:   2024/05/15
-# Last modified:   2024/05/22
+# Last modified:   2024/05/24
 # License:         MIT
 # Copyright (c) 2024 International Atomic Energy Agency (IAEA)
 #
 ############################################################
 
 from .expr_utils.equation_utils import get_variables_in_expr
+from .expr_utils.custom_nodes import VariableToken
 from .expr_utils.tree_walkers import transform_nodes
-from .cpp_types.cpp_varops_query import expr2str_shiftidx
+from .expr_utils.node_trafos import node2str
+from .expr_utils.conversion import convert_to_exprtree
+from .node_checks import is_extvar
 from . import endf2cpp_aux as aux
 from .cpp_types import cpp_varaux
+from .cpp_types.cpp_varops_query import get_idxstr
+from .cpp_types.cpp_dtype_aux import map_dtype
 from . import cpp_primitives as cpp
+from .variable_management import expand_abbreviation
 
 
-def get_expr_value_using_endf_dict(node):
-    variables = get_variables_in_expr(node)
-    valcode = transform_nodes(node, expr2str_shiftidx, dictvars=variables)
+def get_node_value_using_endf_dict(node, dictvar, dtype, vardict, defaults=None):
+    defaults = {} if defaults is None else defaults
+    if not isinstance(node, VariableToken):
+        return node2str(node)
+    dtypestr = map_dtype(dtype)
+    if dtype not in defaults:
+        varname = f'{dictvar}["{node}"]'
+    else:
+        defval = defaults[dtype]
+        varname = f'{dictvar}.attr("get")("{node}", {defval})'
+    idxstrs = []
+    for i, idxtok in enumerate(node.indices):
+        # we retrieve the index values from the
+        # variables in the cpp code and not the dictionary
+        # as they have already been read before
+        curidxstr = get_idxstr(node, i, vardict)
+        idxstrs.append(curidxstr)
+    idxstrs = [f"[py::cast({idxstr})]" for idxstr in idxstrs]
+    extvarname = varname + "".join(idxstrs)
+    return f"py::cast<{dtypestr}>({extvarname})"
+
+
+def get_expr_value_using_endf_dict(node, dictvar, dtype, vardict, defaults=None):
+    node = convert_to_exprtree(node)
+    node = transform_nodes(node, expand_abbreviation, vardict)
+    valcode = transform_nodes(
+        node, get_node_value_using_endf_dict, dictvar, dtype, vardict, defaults
+    )
     return valcode
 
 
@@ -29,7 +60,7 @@ def generate_parse_or_read_verbatim(funname, parse_opts):  # debug
         cpp_varaux.dict_assign(
             "mfmt_dict",
             ["mf", "mt"],
-            f"{funname}_istream(cont, endf_dict, {parse_opts})",
+            f"{funname}_istream(cont, endf_dict[py::cast(mf)][py::cast(mt)], {parse_opts})",
         ),
         cpp.concat(
             [

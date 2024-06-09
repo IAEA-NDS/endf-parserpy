18fa93b27d25b64d838cd2b220b9c9706067f858
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-06-09 07:45:23 +0200

    remove unused/obsolete funcs in `mode_management`

--- endf_parserpy/compiler/mode_management.py
+++ endf_parserpy/compiler/mode_management.py
@@ -3,44 +3,11 @@
 # Author(s):       Georg Schnabel
 # Email:           g.schnabel@iaea.org
 # Creation date:   2024/05/12
-# Last modified:   2024/05/26
+# Last modified:   2024/06/09
 # License:         MIT
 # Copyright (c) 2024 International Atomic Energy Agency (IAEA)
 #
 ############################################################
-
-
-def set_mode(mode, vardict):
-    if mode not in ("read", "write"):
-        raise TypeError("`mode` must be either `read` or `write`")
-    vardict["__mode"] = mode
-
-
-def set_write_mode(vardict):
-    set_mode("write", vardict)
-
-
-def set_read_mode(vardict):
-    set_mode("read", vardict)
-
-
-def get_mode(vardict):
-    curdict = vardict
-    while "__mode" not in curdict and "__up" in curdict:
-        curdict = curdict["__up"]
-    if "__mode" not in curdict:
-        raise TypeError("parser mode has not been set")
-    return curdict["__mode"]
-
-
-def in_read_mode(vardict):
-    mode = get_mode(vardict)
-    return mode == "read"
-
-
-def in_write_mode(vardict):
-    mode = get_mode(vardict)
-    return mode == "write"
 
 
 def _find_parent_dict(varname, vardict):

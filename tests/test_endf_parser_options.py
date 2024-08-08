c7f71d7257880945bd315af7c2deac19a8cc7ec0
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-08-08 19:05:21 +0200

    add tests for ignore_send_records option

--- tests/test_endf_parser_options.py
+++ tests/test_endf_parser_options.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from endf_parserpy.interpreter import EndfParser
 from endf_parserpy.utils.debugging_utils import compare_objects
 from endf_parserpy.utils.user_tools import list_parsed_sections
+from endf_parserpy.interpreter.custom_exceptions import UnexpectedControlRecordError
 
 
 def test_parsefile_include_single_mf_option():
@@ -74,3 +75,67 @@ def test_write_linenum_true_with_verbatim_tapehead():
     assert all(
         r[75:80] == "99999" for r in result if int(r[70:72]) != 0 and int(r[72:75]) == 0
     )
+
+
+def _test_ignore_send_records_false_option(ctrl_start, ctrl_len):
+    endf_file = Path(__file__).parent.joinpath("testdata", "n_2925_29-Cu-63.endf")
+    with open(endf_file, "r") as f:
+        endf_lines = f.readlines()
+    parser = EndfParser(ignore_send_records=False)
+    # remove SEND
+    endf_lines_mod = endf_lines.copy()
+    for i in range(len(endf_lines)):
+        line = endf_lines[i]
+        ctrl = int(line[ctrl_start : ctrl_start + ctrl_len])
+        if ctrl == 0 and i > 0:
+            endf_lines_mod.pop(i)
+            break
+    failed = False
+    try:
+        parser.parse(endf_lines_mod)
+    except UnexpectedControlRecordError:
+        failed = True
+    assert failed
+
+
+def _test_ignore_send_records_true_option(ctrl_start, ctrl_len):
+    endf_file = Path(__file__).parent.joinpath("testdata", "n_2925_29-Cu-63.endf")
+    with open(endf_file, "r") as f:
+        endf_lines = f.readlines()
+    parser1 = EndfParser(ignore_send_records=False)
+    parser2 = EndfParser(ignore_send_records=True)
+    # remove SEND
+    endf_lines_mod = endf_lines.copy()
+    for i in range(len(endf_lines)):
+        line = endf_lines[i]
+        ctrl = int(line[ctrl_start : ctrl_start + ctrl_len])
+        if ctrl == 0 and i > 0:
+            endf_lines_mod.pop(i)
+            break
+    endf_dict1 = parser1.parse(endf_lines)
+    endf_dict2 = parser2.parse(endf_lines_mod)
+    compare_objects(endf_dict1, endf_dict2)
+
+
+def test_ignore_send_records_false_option_with_missing_send_record():
+    _test_ignore_send_records_false_option(72, 3)
+
+
+def test_ignore_send_records_true_option_with_missing_send_record():
+    _test_ignore_send_records_true_option(73, 3)
+
+
+def test_ignore_send_records_false_option_with_missing_fend_record():
+    _test_ignore_send_records_false_option(70, 2)
+
+
+def test_ignore_send_records_true_option_with_missing_fend_record():
+    _test_ignore_send_records_true_option(70, 2)
+
+
+def test_ignore_send_records_false_option_with_missing_mend_record():
+    _test_ignore_send_records_false_option(66, 4)
+
+
+def test_ignore_send_records_true_option_with_missing_mend_record():
+    _test_ignore_send_records_true_option(66, 4)

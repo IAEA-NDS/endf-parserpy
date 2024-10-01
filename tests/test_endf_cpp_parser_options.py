603cd01d310d060fd30e74a21ca085a4cf0f17df
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-10-01 14:49:11 +0200

    implement tests for `ignore_missing_tpid` option of `EndfParserCpp` class

--- tests/test_endf_cpp_parser_options.py
+++ tests/test_endf_cpp_parser_options.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pytest
 from endf_parserpy import EndfParserCpp
 from endf_parserpy.utils.debugging_utils import compare_objects
 from endf_parserpy.utils.user_tools import list_parsed_sections
@@ -36,3 +37,20 @@ def test_write_linenum_true_with_verbatim_tapehead():
     assert all(
         r[75:80] == "99999" for r in result if int(r[70:72]) != 0 and int(r[72:75]) == 0
     )
+
+
+def test_missing_tpid_option_true_with_missing_tpid():
+    parser = EndfParserCpp(ignore_missing_tpid=True)
+    endf_file = Path(__file__).parent.joinpath(
+        "testdata", "malformed_endf", "jeff33_13-Al-27g_mf4_mt2.endf"
+    )
+    endf_dict = parser.parsefile(endf_file)
+
+
+def test_missing_tpid_option_false_with_missing_tpid():
+    parser = EndfParserCpp(ignore_missing_tpid=False)
+    endf_file = Path(__file__).parent.joinpath(
+        "testdata", "malformed_endf", "jeff33_13-Al-27g_mf4_mt2.endf"
+    )
+    with pytest.raises(Exception):
+        endf_dict = parser.parsefile(endf_file)

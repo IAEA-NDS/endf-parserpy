05f2936529fe25f8d1a33a629cdf834d54f18d92
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-10-27 15:44:51 +0100

    introduce `open_section` and `close_section` fun in `endf2cpp_aux_writing` module

--- endf_parserpy/compiler/endf2cpp_aux_writing.py
+++ endf_parserpy/compiler/endf2cpp_aux_writing.py
@@ -3,7 +3,7 @@
 # Author(s):       Georg Schnabel
 # Email:           g.schnabel@iaea.org
 # Creation date:   2024/05/18
-# Last modified:   2024/10/20
+# Last modified:   2024/10/27
 # License:         MIT
 # Copyright (c) 2024 International Atomic Energy Agency (IAEA)
 #
@@ -12,6 +12,8 @@
 
 from . import cpp_primitives as cpp
 from .cpp_types.cpp_dtype_aux import map_dtype
+from .cpp_types.cpp_varops_query import did_read_var, get_idxstr
+from .cpp_types.cpp_varaux import check_variable
 
 
 def prepare_line(linevar, mat, mf, mt, linenum, write_opts):
@@ -69,6 +71,33 @@ def set_tab2_body(linevar, tab_body, mat, mf, mt, linenum, write_opts):
     code = cpp.statement(
         f"write_tab2_body({linevar}, {tab_body}, {mat}, {mf}, {mt}, {linenum}, {write_opts})"
     )
+    return code
+
+
+def open_section(
+    vartok, vardict, current_dict="cpp_current_dict", parent_dict="cpp_parent_dict"
+):
+    check_variable(vartok, vardict)
+    secname = vartok
+    indices = vartok.indices
+
+    idxstrs = []
+    for i, idxtok in enumerate(indices):
+        curidxstr = get_idxstr(vartok, i, vardict)
+        idxstrs.append(curidxstr)
+    idcsarg = "std::vector<int>({" + ", ".join(idxstrs) + "})"
+
+    code = ""
+    code += cpp.statement(f"py::dict {parent_dict} = {current_dict}")
+    code += cpp.statement(
+        f"{current_dict} = "
+        f'cpp_index_shifter_store.setdefault("{secname}", {idcsarg}, py::dict())'
+    )
+    return code
+
+
+def close_section(current_dict="cpp_current_dict", parent_dict="cpp_parent_dict"):
+    code = cpp.statement(f"{current_dict} = {parent_dict}")
     return code
 
 

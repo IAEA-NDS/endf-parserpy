10e0dd0242534581db0d32c71301d738fba1291d
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-11-20 12:06:14 +0100

    add tests for `EndfFloat` comparison methods

---
+++ tests/test_endf_float_class.py
@@ -0,0 +1,36 @@
+import pytest
+from endf_parserpy.utils.math_utils import EndfFloat
+
+
+@pytest.mark.parametrize(
+    "obj1, obj2, relation, expval",
+    [
+        # for equality testing
+        (EndfFloat(7, "7"), 7, lambda x, y: x == y, True),
+        (7, EndfFloat(7, "7"), lambda x, y: x == y, True),
+        (8, EndfFloat(7, "8"), lambda x, y: x == y, False),
+        (EndfFloat(8, "8"), 7, lambda x, y: x == y, False),
+        # for inequality testing
+        (EndfFloat(3, "3"), 9, lambda x, y: x != y, True),
+        (9, EndfFloat(3, "3"), lambda x, y: x != y, True),
+        (3, EndfFloat(3, "3"), lambda x, y: x != y, False),
+        # for smaller-than testing
+        (EndfFloat(3, "3"), 9, lambda x, y: x < y, True),
+        (9, EndfFloat(3, "3"), lambda x, y: x < y, False),
+        # for larger-than testing
+        (EndfFloat(3, "3"), 9, lambda x, y: x > y, False),
+        (9, EndfFloat(3, "3"), lambda x, y: x > y, True),
+        # for smaller-equal testing
+        (EndfFloat(3, "3"), 9, lambda x, y: x <= y, True),
+        (9, EndfFloat(3, "3"), lambda x, y: x <= y, False),
+        (EndfFloat(3, "3"), 3, lambda x, y: x <= y, True),
+        (3, EndfFloat(3, "3"), lambda x, y: x <= y, True),
+        # for larger-equal testing
+        (EndfFloat(3, "3"), 9, lambda x, y: x >= y, False),
+        (9, EndfFloat(3, "3"), lambda x, y: x >= y, True),
+        (EndfFloat(3, "3"), 3, lambda x, y: x >= y, True),
+        (3, EndfFloat(3, "3"), lambda x, y: x >= y, True),
+    ],
+)
+def test_relations(obj1, obj2, relation, expval):
+    assert relation(obj1, obj2) == expval

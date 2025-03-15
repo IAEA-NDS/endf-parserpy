33580644689c2909084a70e6e4236740d95bfc51
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-15 14:45:17 +0100

    fix mf1/mt460 recipe

--- endf_parserpy/endf_recipes/endf6/endf_recipe_mf1_mt460.py
+++ endf_parserpy/endf_recipes/endf6/endf_recipe_mf1_mt460.py
@@ -3,7 +3,7 @@
 # Author(s):       Georg Schnabel
 # Email:           g.schnabel@iaea.org
 # Creation date:   2022/11/20
-# Last modified:   2024/04/26
+# Last modified:   2025/03/15
 # License:         MIT
 # Copyright (c) 2022-2024 International Atomic Energy Agency (IAEA)
 #
@@ -14,7 +14,7 @@ ENDF_RECIPE_MF1_MT460 = """
 if LO==1 [lookahead=1]:
     [MAT,1,460/ ZA, AWR, LO, 0, NG, 0]HEAD
     for i=1 to NG:
-        [MAT,1,460/ E[i], 0.0, i, 0, NR, NP / tint[i] / T[i] ]TAB1
+        [MAT,1,460/ E[i], 0.0, i, 0, NR, NP / tint / T ]TAB1 (table[i])
     endfor
 
 elif LO==2 [lookahead=1]:

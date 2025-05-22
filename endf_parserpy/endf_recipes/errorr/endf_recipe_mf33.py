7e5753dbbddbdf896d490d09e64f23924d81e22a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-05-22 16:28:40 +0200

    introduce MTL flag in `ERRORR` MF33 recipe

--- endf_parserpy/endf_recipes/errorr/endf_recipe_mf33.py
+++ endf_parserpy/endf_recipes/errorr/endf_recipe_mf33.py
@@ -13,13 +13,17 @@ ENDF_RECIPE_MF33 = """
 # MF33 recipe for and ERRORR covariance tape (file) prepared using NJOY2016.
 # MF33/MT is used to specify the group averaged covariance matrix.
 
-# var MAT:       Material number
-# var MT:        Reaction number according to ENDF conventions
-# var ZA:        ZA = (1000.0 * Z) + A = ZA number
-#                Z ... charge number of material
-#                A ... mass number of material
-# var AWR:       Ratio of the mass of the material to that of the neutron
-# var NK:        Number of sections (MAT,MT; MAT1,MT1)
+# var MAT:  Material number
+# var MT:   Reaction number according to ENDF conventions
+# var ZA:   ZA = (1000.0 * Z) + A = ZA number
+#           Z ... charge number of material
+#           A ... mass number of material
+# var AWR:  Ratio of the mass of the material to that of the neutron
+# var MTL:  Non-zero value of MTL is used as a flag to indicate that reaction MT is
+#           one component of the evaluator-defined lumped reaction MTL, as discussed
+#           in paragraphs at the end of Section 33.2 and 33.3 below; in this case, no
+#           covariance information subsections are given for reaction MT and NL=0.
+# var NK:   Number of sections (MAT,MT; MAT1,MT1)
 #
 # var subsection/*/MAT1:      MAT number of the second reaction in the pair
 # var subsection/*/MT1:       MT number of the second reaction in the pair
@@ -37,7 +41,7 @@ ENDF_RECIPE_MF33 = """
 # given covariance matrix and signals that the next card (line) will name a
 # new reaction pair.
 
-[MAT, 33, MT/ ZA, AWR, 0, 0, 0, NK] CONT
+[MAT, 33, MT/ ZA, AWR, 0, MTL, 0, NK] CONT
 for k=1 to NK:
     (subsection[k])
         [MAT, 33, MT/ 0.0, 0.0, MAT1, MT1, 0, NG] CONT

af4abfef22719492be7438642fc90d65e8e8b461
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-05-28 18:13:54 +0200

    fix recipe element tests and add roundtrip test

--- tests/base_test_endf_recipe_elements.py
+++ tests/base_test_endf_recipe_elements.py
@@ -1,9 +1,10 @@
 import pytest
 from endf_parserpy import EndfParser
+from endf_parserpy.utils.debugging_utils import compare_objects
 
 
 def create_mf1_test_section(mt, inconsistent=False):
-    ctrl_record = "1234 1  1"
+    ctrl_record = "1234 1" + str(mt).rjust(3)
     head_record = (
         (
             "0.0".rjust(11)
@@ -46,6 +47,12 @@ def create_mf1_test_section(mt, inconsistent=False):
 
 
 @pytest.fixture(scope="function")
+def mf1_mt_test_section(request):
+    mt = request.param
+    return create_mf1_test_section(mt, inconsistent=False)
+
+
+@pytest.fixture(scope="function")
 def mf1_mt1_test_section():
     return create_mf1_test_section(mt=1, inconsistent=False)
 
@@ -65,28 +72,33 @@ def mf1_mt3_test_section():
     return create_mf1_test_section(mt=3, inconsistent=False)
 
 
-def mf1_test_section_assertions(endf_dict):
-    assert endf_dict[1][1]["A"][1] == 9
-    assert endf_dict[1][1]["A"][2] == 2
-    assert endf_dict[1][1]["B"][1] == 2
-    assert endf_dict[1][1]["B"][2] == 4
-    assert endf_dict[1][1]["C"][1] == 5
-    assert endf_dict[1][1]["C"][2] == 6
+def mf1_test_section_assertions(endf_dict, mt):
+    assert endf_dict[1][mt]["A"][1] == 9
+    assert endf_dict[1][mt]["A"][2] == 2
+    assert endf_dict[1][mt]["B"][1] == 2
+    assert endf_dict[1][mt]["B"][2] == 4
+    assert endf_dict[1][mt]["C"][1] == 5
+    assert endf_dict[1][mt]["C"][2] == 6
 
 
 def test_nested_indices_in_record_fields(mf1_mt1_test_section, endf_parser):
     endf_dict = endf_parser.parse(mf1_mt1_test_section)
-    mf1_test_section_assertions(endf_dict)
+    mf1_test_section_assertions(endf_dict, mt=1)
 
 
 def test_nested_indices_in_if_head(mf1_mt2_test_section, endf_parser):
-    parser = EndfParser(
-        endf_format="test", ignore_send_records=True, ignore_missing_tpid=True
-    )
-    endf_dict = parser.parse(mf1_mt2_test_section)
-    mf1_test_section_assertions(endf_dict)
+    endf_dict = endf_parser.parse(mf1_mt2_test_section)
+    mf1_test_section_assertions(endf_dict, mt=2)
 
 
 def test_nested_indices_in_for_head(mf1_mt3_test_section, endf_parser):
     endf_dict = endf_parser.parse(mf1_mt3_test_section)
-    mf1_test_section_assertions(endf_dict)
+    mf1_test_section_assertions(endf_dict, mt=3)
+
+
+@pytest.mark.parametrize("mf1_mt_test_section", [1, 2, 3], indirect=True)
+def test_endf_roundtrip(mf1_mt_test_section, endf_parser):
+    endf_dict = endf_parser.parse(mf1_mt_test_section)
+    sec_out = endf_parser.write(endf_dict)
+    endf_dict2 = endf_parser.parse(sec_out)
+    compare_objects(endf_dict, endf_dict2)

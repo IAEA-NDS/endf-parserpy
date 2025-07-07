a96102be460277701d3f54c5b1b9f07c33a9b09c
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-07-07 09:36:22 +0200

    add test to `test_py_and_cpp_parser_output_equivalence`

--- tests/test_py_and_cpp_parser_output_equivalence.py
+++ tests/test_py_and_cpp_parser_output_equivalence.py
@@ -62,3 +62,36 @@ def test_fixdec_rounding_behavior_equivalence(mf3_section):
     endftext_py = parser_py.write(mf3_section)
     endftext_cpp = parser_cpp.write(mf3_section)
     assert endftext_py == endftext_cpp
+
+
+def test_all_writing_options_equivalence(mf3_section):
+    def generate_params():
+        for prefer_noexp in (True, False):
+            for abuse_signpos in (True, False):
+                for skip_intzero in (True, False):
+                    for keep_E in (True, False):
+                        yield {
+                            "prefer_noexp": prefer_noexp,
+                            "abuse_signpos": abuse_signpos,
+                            "skip_intzero": skip_intzero,
+                            "keep_E": keep_E,
+                        }
+
+    def generate_numbers():
+        for pot in (-150, -100, -99, -11, -10, -9, -5, 0, 5, 9, 10, 11, 99, 100, 150):
+            for i in range(1000):
+                yield i / 100 * (10**pot)
+
+    numbers = tuple(generate_numbers())
+    numbers2 = tuple(1 + n for n in numbers)
+    number = numbers + numbers2
+    for params in generate_params():
+        parser_py = EndfParserPy(**params)
+        parser_cpp = EndfParserCpp(**params)
+        mf3_section[3][1]["INT"] = [2]
+        mf3_section[3][1]["NBT"] = [len(numbers)]
+        mf3_section[3][1]["xstable"]["xs"] = numbers
+        mf3_section[3][1]["xstable"]["E"] = numbers
+        endftext_py = parser_py.write(mf3_section)
+        endftext_cpp = parser_py.write(mf3_section)
+        assert endftext_py == endftext_cpp
